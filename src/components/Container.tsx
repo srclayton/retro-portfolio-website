9e90014b4047ab992b59020a7c6c805f9a828d41
Clayton <clayton@srocha.dev>
2023-09-20 14:50:54 -0300

    Commit inicial.

---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+import "./Container.css";
+function Container() {
+  return (
+    <div className="container boxShadow">
+      <div className="osNavbar">
+        <div className="osName">@SROCHA.OS</div>
+        <div className="osInfo">CONFIG 2023 *** SEP 20-09 ***</div>
+        <div className="osGeo">02:39PM (GMT-3) | @ BRA</div>
+      </div>
+      <div className="osBody">dsadsa</div>
+    </div>
+  );
+}
+
+export default Container;
